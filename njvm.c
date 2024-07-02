45c2feb9a9eca5e11443994c329b45818b751e7b
aafn <alexander.afanasjew@mni.thm.de>
2024-07-02 03:38:17 +0200

    Created support fuctions for the bigint-library

--- njvm.c
+++ njvm.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <support.h>
+#include <bigint.h>
 
 #define intValue(s) (*(int*)(s.u.objRef->data))
 #define charValue(s) (*(char*)(s.u.objRef->data))
@@ -18,6 +20,7 @@ typedef struct {
   unsigned int size;    /* byte count of payload data */
   unsigned char data[1]; /* payload data, size as needed */
 }*ObjRef;
+
 
 typedef struct{
     int isObjRef;   /* slot used for object reference ? */
@@ -52,6 +55,7 @@ ObjRef createInt(int value) {
     *(int*)obj->data = value;
     return obj;
 }
+
 ObjRef createChar(char value) {
     ObjRef obj = (ObjRef) malloc(sizeof(unsigned int) + sizeof(char));
     if (obj == NULL) {
@@ -61,6 +65,45 @@ ObjRef createChar(char value) {
     obj->size = sizeof(char);
     *(char*)obj->data = value;
     return obj;
+}
+
+/*
+ * This routine is called in case a fatal error has occurred.
+ * It should print the error message and terminate the program.
+ */
+void fatalError(char *msg) {
+  printf("Fatal error: %s\n", msg);
+  exit(1);
+}
+
+
+/*
+ * This function is called whenever a new primitive object with
+ * a certain amount of internal memory is needed. It should return
+ * an object reference to a regular object, which contains a freely
+ * usable memory area of at least the requested size (measured in
+ * bytes). The memory area need not be initialized in any way.
+ *
+ * Note that this function may move all objects in memory at will
+ * (due to, e.g., garbage collection), as long as the pointers in
+ * the global "bip" structure point to the correct objects when
+ * the function returns.
+ */
+void * newPrimObject(int dataSize) {
+  ObjRef bigObjRef;
+
+  bigObjRef = malloc(sizeof(unsigned int) +
+                  dataSize * sizeof(unsigned char));
+  if (bigObjRef == NULL) {
+    fatalError("newPrimObject() got no memory");
+  }
+  bigObjRef->size = dataSize;
+  return bigObjRef;
+}
+
+void * getPrimObjectDataPointer(void * obj){
+    ObjRef oo = ((ObjRef)  (obj));
+    return oo->data;
 }
 
 void clearStackSlot(StackSlot *slot) {
